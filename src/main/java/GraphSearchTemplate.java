dd57eb909ff2aa0c0a1cc0bd0f4003270f4ee53c
Lauryn Dake <121473566+lsdake@users.noreply.github.com>
2025-05-01 13:56:27 -0700

    Update GraphSearchTemplate.java

--- src/main/java/GraphSearchTemplate.java
+++ src/main/java/GraphSearchTemplate.java
@@ -12,41 +12,71 @@ public abstract class GraphSearchTemplate {
     protected Map<String, PathNode> nodeMap;
     protected Deque<PathNode> frontier;
 
-    
     // template method, defines the skeleton of the graph search
     
     public Path search(DotGraph graph, String src, String dst) {
+
+        // handling lowercase inputs Just In Case lol
+        src = src.toUpperCase();
+        dst = dst.toUpperCase();
+
         this.graph = graph;
-        initNodeMap();
+        nodeMap = initNodeMap();
         frontier = initFrontier();
 
         PathNode start = nodeMap.get(src);
         start.setExplored(true);
         frontierAdd(start);
+        PathNode current = start;
 
         while (!frontier.isEmpty()) {
-            PathNode current = frontierRemove();
+            current = frontierRemove();
+
+            // for printing for demo
+            Path prefix = buildPath(current);
+            onVisit(prefix.getPathArray());
+
             if (current.getName().equals(dst)) {
-                return buildPath(current);
+                Path p = buildPath(current);
+                p.setDestinationReached();
+                return p;
             }
-            for (String neighbor : graph.getNeighbors(current.getName())) {
-                PathNode adj = nodeMap.get(neighbor);
-                if (!adj.isExplored()) {
-                    adj.setExplored(true);
-                    adj.setParent(current.getName());
-                    frontierAdd(adj);
+
+            // collect visited names for planNext
+            List<String> visited = new ArrayList<>();
+            for (PathNode pn : frontier) visited.add(pn.getName());
+            visited.add(current.getName());
+
+            for (String neighbor : planNext(current.getName(), visited)) {
+                PathNode adj = nodeMap.get(neighbor.toUpperCase());
+                if (adj != null) {
+                    if (!adj.isExplored()) {
+                        adj.setExplored(true);
+                        adj.setParent(current.getName());
+                        frontierAdd(adj);
+                    }
+                }
+                else {
+                    return buildPath(current);
                 }
             }
         }
-        return null;
+        return buildPath(current);
+    }
+
+    protected List<String> planNext(String current, List<String> visitedNames) {
+        return graph.getNeighbors(current);
     }
 
     // init the nodeMap from the graph nodes
-    private void initNodeMap() {
-        nodeMap = new HashMap<>();
+    private Map<String, PathNode> initNodeMap() {
+        Map<String, PathNode> map = new HashMap<>();
         for (String name : graph.getNodes()) {
-            nodeMap.put(name, new PathNode(name));
+
+            name = name.toUpperCase();
+            map.put(name, new PathNode(name));
         }
+        return map;
     }
 
     // rebuilds the path by going from child to parent till it reaches the end
@@ -63,4 +93,8 @@ public abstract class GraphSearchTemplate {
     protected abstract Deque<PathNode> initFrontier();
     protected abstract void frontierAdd(PathNode node);
     protected abstract PathNode frontierRemove();
+    
+    protected void onVisit(String pathSoFar) {
+        // no operation by default
+    }
 }
